a481b60a1b15504e986506409d0937d100cfff3d
Ruslan <89604587433@mail.ru>
2022-08-31 18:04:01 +0300

    Example 006

---
+++ Ex_006/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя");
+string UserName = Console.ReadLine();
+if (UserName.ToLower()=="маша")
+{
+    Console.WriteLine("Ура это же Маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(UserName);
+}
